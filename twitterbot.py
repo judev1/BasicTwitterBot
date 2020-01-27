6a80622e5b468e7ac3f5a6c7a81802edc9dfa1a8
Zynox <jude.version1.0@gmail.com>
2020-01-27 23:57:36 +0000

    Update 1.0.1

--- twitterbot.py
+++ twitterbot.py
@@ -1,27 +1,15 @@
- # TwitterBot - Judev1
+# TwitterBot - Judev1
+
 
  # Setup
 import time
 import sys
+valid1 = False
+valid2 = False
 retry = 0
 validate = 0
-
-
 print(u"Welcome to my Twitter Bot!")
 print(u"Running...")
-
-
- # Message
- # Posting the same message directly after each other will not work
- # Default "Hello World"
-message = "Hello World"
-
-
- # Twitter keys
-cons_key = ""
-cons_secret = ""
-access_token = ""
-access_token_secret = ""
 
 
  # Runs - how many times do you want the program to repeat
@@ -38,58 +26,79 @@ runs = 1
  # Default 21600 seconds
 frequency = 21600
 
+      
+ # Checks that the frequency is a positive integer
+try:
+    frequency = int(frequency)
+    if frequency < 0:
+        print(u"Your frequency isn't a positive integer")
+        print("Killed program")
+        sys.exit()
+except:
+    print(u"Your frequency isn't a positive integer")
+    print("Killed program")
+    sys.exit()
+
+				
+ # Twitter keys
+cons_key = ""
+cons_secret = ""
+access_token = ""
+access_token_secret = ""
+
 
  # Install Twitter
 try:
     import twitter
+
     dwnld = True
     print(u"Successfully installed Twitter")
 except:
     dwnld = False
     print(u"Failed to install Twitter")
 
-
+	
  # Setups the Twitter authenification
-auth = twitter.OAuth( access_token, access_token_secret, cons_key, cons_secret)
+auth = twitter.OAuth(access_token, access_token_secret, cons_key, cons_secret)
 t = twitter.Twitter(auth=auth)
 
 
- # Checks that the frequency is a positive integer
-try:
-    frequency = int(frequency)
-    if frequency < 0:
-        print(u"The frequency wasn't a positive integer")
-        print("Killed program")
-        sys.exit()
-except:
-    print(u"Your frequency wasn't a positive integer")
-    print("Killed program")
-    sys.exit()
-
 
 totalruns = runs
-totaltime = (frequency*runs) - frequency
+totaltime = (frequency * runs) - frequency
+
+
 if dwnld == True:
     while retry > 3 or runs > 0:
         success = False
-        # Tweeting
+				
+				     # Tweeting
         try:
+            plot = storygen.plot()
+            message = plot
             t.statuses.update(status=message)
-             # Add extensions here
             success = True
+											
         except:
             print(u"Unable to tweet")
+										
             if retry < 3:
                 retry = retry + 1
                 print(u"Attempting to retry")
                 time.sleep(2)
+															
             else:
                 print("Killed program")
                 sys.exit()
+															
         if success == True:
             print(u"Successfully tweeted")
             print(u"Tweeted:", message)
             runs = runs - 1
-            print(u"Next tweet in {} seconds".format(frequency))
-            time.sleep(frequency)
+												
+            if runs <= 0:
+												    print(u"Next tweet in {} seconds".format(frequency))
+                time.sleep(frequency)
+
+												
 print("Program Finished! Posted {} times, elapsed time {} seconds".format(totalruns, totaltime))
